b7b9c6278dfb8a56de6c9cd343371980ef5bcf1b
DysonCarter <129555170+DysonCarter@users.noreply.github.com>
2024-05-14 15:13:27 -0400

    Add Typing

--- script.js
+++ script.js
@@ -1,40 +1,51 @@
 // Get all cells
 const cells = document.querySelectorAll('.cell');
 
-// On Click for each cell
+var toggle = true;
+
 cells.forEach(cell => {
-    var toggle = true;
+    cell.setAttribute('tabindex', '0');
+
+    // On Click for each cell
     cell.addEventListener('click', function() {
-        // Get the index of the clicked cell
+        // Get Consts
         const index = parseInt(this.getAttribute('data-index'));
-        const rowStart = Math.floor((index - 1) / 5) * 5 + 1;
+        const rowStart = Math.floor((index) / 5) * 5;
         const rowEnd = rowStart + 4;
 
+        // Handle toggling
+        if(this.style.backgroundColor == 'yellow'){
+            toggle = !toggle;
+        }
+        
         // Handle color change
         if (toggle == true) {
-        cells.forEach(c => {
-            const cIndex = parseInt(c.getAttribute('data-index'));
-            if (cIndex >= rowStart && cIndex <= rowEnd) {
-            c.style.backgroundColor = 'grey';
-            } else {
-            c.style.backgroundColor = 'white';
-            }
-        });
-        this.style.backgroundColor = 'yellow';
-        toggle = false;
+            cells.forEach(c => {
+                const cIndex = parseInt(c.getAttribute('data-index'));
+                if (cIndex >= rowStart && cIndex <= rowEnd) {
+                c.style.backgroundColor = 'grey';
+                } else {
+                c.style.backgroundColor = 'white';
+                }
+            });
+            this.style.backgroundColor = 'yellow';
         } else {
-        cells.forEach(c => {
-            const cIndex = parseInt(c.getAttribute('data-index'));
-            if (cIndex % 5 === index % 5) {
-            c.style.backgroundColor = 'grey';
-            } else {
-            c.style.backgroundColor = 'white';
-            }
-        });
-        this.style.backgroundColor = 'yellow';
-        toggle = true;
+            cells.forEach(c => {
+                const cIndex = parseInt(c.getAttribute('data-index'));
+                if (cIndex % 5 === index % 5) {
+                c.style.backgroundColor = 'grey';
+                } else {
+                c.style.backgroundColor = 'white';
+                }
+            });
+            this.style.backgroundColor = 'yellow';
         }
     });
 
-
+    //For Pressing Keys
+    cell.addEventListener("keydown", function(event){
+        const key = event.key;
+        if(key.length === 1)
+            this.innerHTML = key.toUpperCase();
+    })
 });
