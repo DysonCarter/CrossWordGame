f9f181bba3f3db432582acce40e3ce4881a5eb4e
DysonCarter <129555170+DysonCarter@users.noreply.github.com>
2024-05-17 15:06:35 -0400

    Add ios support

--- script.js
+++ script.js
@@ -246,7 +246,7 @@ function moveToPreviousCell(currentIndex) {
 }
 
 function isMobileDevice() {
-    return /Mobi|Android/i.test(navigator.userAgent);
+    return /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 }
 
 function addEventListeners() {
